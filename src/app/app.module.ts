468f43d4729cbe613c81fb8ecf6eead64698dc7d
Roman-Liubinets <roman.liubinets@gmail.com>
2018-06-26 23:18:06 +0300

    import RouterModule for registration out paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { FirstPageComponent } from './first-page/first-page.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 import { ThirdPageComponent } from './third-page/third-page.component';
-import { Routes } from '@angular/router';  
+import { Routes, RouterModule } from '@angular/router';  
 
 const appRoute: Routes = [
   { path: 'firstPage', component: FirstPageComponent },
