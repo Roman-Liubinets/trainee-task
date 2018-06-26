bd9e3375d2689d7717d6aeb2066b503e6b798f5f
Roman-Liubinets <roman.liubinets@gmail.com>
2018-06-26 23:07:41 +0300

    create clear trainee Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent
   ],
   imports: [
     BrowserModule
