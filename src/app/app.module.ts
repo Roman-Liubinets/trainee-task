c9f385e03ce6a44042081e7a94eae69cdc7df986
Roman-Liubinets <roman.liubinets@gmail.com>
2018-06-26 23:10:43 +0300

    create array for Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FirstPageComponent } from './first-page/first-page.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 import { ThirdPageComponent } from './third-page/third-page.component';
 
+const appRoutes = [];
+
 @NgModule({
   declarations: [
     AppComponent,
