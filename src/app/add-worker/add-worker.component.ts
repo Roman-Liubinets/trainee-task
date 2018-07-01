f8e1f305d6469f13ba47c8c827dfde8ab91145a8
Roman-Liubinets <roman.liubinets@gmail.com>
2018-07-01 18:53:13 +0300

    Now we can add workers to with service functions

--- src/app/add-worker/add-worker.component.ts
+++ src/app/add-worker/add-worker.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { CrudService } from './../crud.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-worker',
   templateUrl: './add-worker.component.html',
   styleUrls: ['./add-worker.component.css']
 })
-export class AddWorkerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AddWorkerComponent {
+  workerName="";
+  addWorker() {
+    this.crudService.addWorker(this.workerName);
+    this.workerName = "";
   }
+
+  constructor(private crudService: CrudService) { }
+
 
 }
